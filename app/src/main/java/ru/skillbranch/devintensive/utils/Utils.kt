20ed3ac448915b75fbd1a712e18237c56201cbbf
levgeogr <levgeogr@gmail.com>
2019-07-12 23:11:16 +0300

    transliteration

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,6 +1,7 @@
 package ru.skillbranch.devintensive.utils
 
 object Utils {
+
     fun parseFullName(fullName: String?): Pair<String?, String?> {
         var names: List<String>? = fullName?.split(" ")
 
@@ -18,7 +19,7 @@ object Utils {
         return Pair(firstName, lastName)
     }
 
-    fun String.customFirst() :String = when {
+    fun String.customFirst(): String = when {
         (trim() == "") -> ""
         else -> trim().first().toString()
     }
@@ -30,9 +31,24 @@ object Utils {
     }
 
 
-    fun transliteration(payload: String?, divider: String = " ") {
-
+    fun transliteration(payload: String?, divider: String = " "): String {
+        var res: String = ""
+        payload?.forEach {
+            res += when {
+                (it == ' ') -> divider
+                it.isUpperCase() -> bukovkiEpt[it.toLowerCase()]?.capitalize() ?: it.toString()
+                else -> bukovkiEpt[it] ?: it.toString()
+            }
+        }
+        return res
     }
 
 
+    private val bukovkiEpt = mapOf(
+        'а' to "a", 'б' to "b", 'в' to "v", 'г' to "g", 'д' to "d", 'е' to "e", 'ё' to "e",
+        'ж' to "zh", 'з' to "z", 'и' to "i", 'й' to "i", 'к' to "k", 'л' to "l", 'м' to "m",
+        'н' to "n", 'о' to "o", 'п' to "p", 'р' to "r", 'с' to "s", 'т' to "t", 'у' to "u",
+        'ф' to "f", 'х' to "h", 'ц' to "c", 'ч' to "ch", 'ш' to "sh", 'щ' to "sh'",
+        'ъ' to "", 'ы' to "i", 'ь' to "", 'э' to "e", 'ю' to "yu", 'я' to "ya"
+    )
 }
