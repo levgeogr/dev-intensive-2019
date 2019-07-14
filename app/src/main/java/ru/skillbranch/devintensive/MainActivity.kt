d8020ff74767118ec1d6812a3054e4aa11be26c9
levgeogr <levgeogr@gmail.com>
2019-07-15 02:29:40 +0300

    update gradle, Bender class, markup upgrade

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,12 +1,25 @@
 package ru.skillbranch.devintensive
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var etMessage :EditText
+    lateinit var ivBender : ImageView
+    lateinit var tvText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etMessage = et_message
+        ivBender = iv_bender
+        tvText = tv_text
+
     }
 }
---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,15 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+
+fun Activity.hideKeyboard() {
+
+}
+
+fun Activity.isKeyboardOpen () {
+
+}
+
+fun Activity.isKeyboardClosed() {
+
+}
